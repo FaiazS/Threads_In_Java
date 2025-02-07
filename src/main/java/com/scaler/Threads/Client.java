e9829bbfa5700a207d7f885c4b51debe8a698d7b
Faiaz Ahmed <69419309+FaiazS@users.noreply.github.com>
2025-02-08 01:16:07 +0530

    8/2/2025 - Basic Threads/Tasks (
    1 - Adding and Printing Numbers from 1 to 100 and 1 to N.
    
    2 - Printing Numbers from 1 to N each on a separate Thread.
    
    3 - Performing tasks using Thread Pooling via
    
    Executors, Executor Service.
    
    )
    
    Implemented.

--- src/main/java/com/scaler/Threads/Client.java
+++ src/main/java/com/scaler/Threads/Client.java
@@ -1,5 +1,8 @@
 package com.scaler.Threads;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Client {
 
     public static void main(String[] args) {
@@ -22,6 +25,13 @@ public class Client {
 
              Thread thread2 = new Thread(new NumberPrinter(i));
              thread2.start();
+         }
+
+         ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+         for(int i = 1; i <= number; i++){
+
+             executorService.execute(new Thread(new NumberPrinter(i)));
          }
     }
 }
